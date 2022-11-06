618f8c8e2a6ab7788f0940b0acf6c2e38bafaf9d
ramadlana <hidayahweb@gmail.com>
2022-11-06 14:30:06 +0700

    server to 80

--- server.py
+++ server.py
@@ -4,4 +4,4 @@ from src.main import app
 # in development: uvicorn server:app --reload
 # in production: python server.py
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
